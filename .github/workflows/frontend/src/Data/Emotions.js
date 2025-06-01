95f47e36899e04f561f0f042facb619780f63346
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-06-01 12:33:01 +0530

    Create Emotions.js

---
+++ .github/workflows/frontend/src/Data/Emotions.js
@@ -0,0 +1,10 @@
+// frontend/src/data/emotions.js
+
+export const emotionColors = {
+  happy: "#FFD93D",
+  sad: "#6C63FF",
+  calm: "#88CCF1",
+  focused: "#38B000",
+  stressed: "#FF5C5C",
+  excited: "#FFB703",
+};
